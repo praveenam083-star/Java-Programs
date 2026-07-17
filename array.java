e8f02bb659a765cde66a3a811d559a95218ef95a
praveena <praveenam083@gmail.com>
2026-07-17 19:49:03 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,16 @@
+ public class array {
+public static void main(String[] args) {
+    int arr[]={10,20,30,40};
+    String name="praveena";
+    int age=23;
+    
+    
+    System .out.println(name) ;  
+        System .out.println(age) ; 
+        
+        
+    }   
+    }
+   
+
+
